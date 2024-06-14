14a35d8ace40fffe308b64a259b3204d406f3018
Joel Nises <joel.nises@embark-studios.com>
2024-06-14 10:46:04 +0200

    Fix lints that rust 1.79 will start complaining about (#12811)
    numerical constants should no longer start with `std::`
    
    https://github.com/EmbarkStudios/wim-app/pull/12811

--- src/bounding_box.rs
+++ src/bounding_box.rs
@@ -34,8 +34,8 @@ impl BoundingBox {
     #[inline]
     pub fn nothing() -> Self {
         Self {
-            min: Vec3::splat(core::f32::INFINITY),
-            max: Vec3::splat(core::f32::NEG_INFINITY),
+            min: Vec3::splat(f32::INFINITY),
+            max: Vec3::splat(f32::NEG_INFINITY),
         }
     }
 
@@ -43,8 +43,8 @@ impl BoundingBox {
     #[inline]
     pub fn everything() -> Self {
         Self {
-            min: Vec3::splat(core::f32::NEG_INFINITY),
-            max: Vec3::splat(core::f32::INFINITY),
+            min: Vec3::splat(f32::NEG_INFINITY),
+            max: Vec3::splat(f32::INFINITY),
         }
     }
 
